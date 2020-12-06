81205dedea5fc2deab1869b76aa470f63555b0bd
Sergey Luchko <sergey.luchko@corp.mail.ru>
2020-12-06 23:04:02 +0300

    fix

--- configs/yandex_speech_kit_config.py
+++ configs/yandex_speech_kit_config.py
@@ -1,5 +1,5 @@
 class YandexConfig:
-    _AM_TOKEN = "t1.9euelZqOmJ7Mlp3Jm5rHk56PkZGViu3rnpWax8eLjpnLzsmcnZ3PyI2Zlpnl8_dNPksB-u8_QBY2_t3z9w1tSAH67z9AFjb-.biw-uouvfcZOMUHZ5AZDSF2-mo4eNF_wCnsS1t4AZG0SnBzAxeSUcjWQvEJXYevDnUY-NeVjcsI11AV8Hp1ZBw"
+    _AM_TOKEN = ""
     _FOLDER_ID = "b1gof8ni37m9mdsnkabm"
     _VOICE = "filipp",
     _EMOTION = "good"
