0395d8905f07c8d3d7512ea8bc7c8ca0226b97e5
Sergey Luchko <sergey.luchko@corp.mail.ru>
2021-01-17 22:06:49 +0300

    rewrite

--- main.py
+++ main.py
@@ -48,4 +48,4 @@ async def route_to_speech(request_body: RouteToSpeechRequestBody):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8091, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8091, log_level="info")
